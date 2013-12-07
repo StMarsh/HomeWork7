74f3191dd28b1c5d2381583e0cd18320521f74fe
StMarsh <milano_@mail.ru>
2013-12-07 21:30:27 +0200

    in work

---
+++ components/com_student_list/views/rss/tmpl/default.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @version     1.0.0
+ * @package     com_student_list
+ * @copyright   © 2013. Все права защищены.
+ * @license     GNU General Public License версии 2 или более поздней; Смотрите LICENSE.txt
+ * @author      StMarsh <milano_@mail.ru> - http://vk.com/id5666638
+ */
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+?>
